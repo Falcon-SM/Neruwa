b979fb805c250128e33e58d52437021d93443ffc
Falcon-SM <hayabusamtmt@gmail.com>
2026-07-12 19:00:31 +0900

    Initial Commit

---
+++ Sleeper/SleeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleeperApp.swift
+//  Sleeper
+//
+//  Created by Shun Matsumoto on 2026/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SleeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
